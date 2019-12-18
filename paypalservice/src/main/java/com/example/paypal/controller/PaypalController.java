935cf070d0141ae0dc087518a218fa4b061b3791
elena1237 <elenakevac@gmail.com>
2019-12-18 12:26:33 +0100

    method for get json

--- paypalservice/src/main/java/com/example/paypal/controller/PaypalController.java
+++ paypalservice/src/main/java/com/example/paypal/controller/PaypalController.java
@@ -2,6 +2,7 @@ package com.example.paypal.controller;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
@@ -17,6 +18,12 @@ public class PaypalController {
     @RequestMapping("/")
     public String helloFromTestApi(){
         return "Paypal service. Hello from instance = " + instance;
+    }
+
+    @RequestMapping(value="getjson",method = RequestMethod.GET)
+    public  String getJSON()
+    {
+        return "JSON from Paypal service. Hello from instance = " + instance;
     }
 }
 
